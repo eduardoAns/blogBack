16dc6cd3560fb83f41480cf6e48842dbe7c64b90
Eduardo Ansa <eduardoansa@gmail.com>
2022-09-01 19:04:55 -0400

    integrando login con jwt

--- src/main/java/com/example/blogapi/controllers/AuthController.java
+++ src/main/java/com/example/blogapi/controllers/AuthController.java
@@ -2,15 +2,18 @@ package com.example.blogapi.controllers;
 
 
 import com.example.blogapi.DAO.UsuarioDao;
+import com.example.blogapi.exceptions.NotFoundException;
 import com.example.blogapi.models.Usuario;
 import com.example.blogapi.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
 public class AuthController {
 
     @Autowired
@@ -19,15 +22,21 @@ public class AuthController {
     @Autowired
     private JWTUtil jwtUtil;
 
-    @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public Map<String, Map<String, String>> login(@RequestBody Usuario usuario) {
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseBody
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public Map<String, String> login(@RequestBody Usuario usuario) {
+
+        if(usuarioDao.obtenerUsuarioPorCredenciales(usuario) == null){
+            throw new NotFoundException("Usuario o contraseña incorrectos","P-404");
+        }
         Usuario usuarioLogueado = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
         return createToken(usuarioLogueado, usuarioLogueado.getId());
     }
 
     @RequestMapping(value = "api/validate-token", method = RequestMethod.GET)
-    public Map<String, Map<String, String>> validateToken(@RequestHeader(value = "Authorization") String tokenUsuario) {
+    public Map<String, String> validateToken(@RequestHeader(value = "Authorization") String tokenUsuario) {
 
         System.out.println(tokenUsuario);
         String usuarioId = jwtUtil.getKey(tokenUsuario);
@@ -59,18 +68,15 @@ public class AuthController {
     }
 
 
-    private Map<String, Map<String, String>> createToken(Usuario usuarioLogueado, int id) {
 
+
+    private  Map<String, String> createToken(Usuario usuarioLogueado, int id) {
 
         String usuarioCorreo = usuarioLogueado.getEmail();
         String usuarioNombre = usuarioLogueado.getNombre();
         String usuarioRol = usuarioLogueado.getRol();
 
-
-        Map<String, Map<String, String>> map = new HashMap<String, Map<String, String>>();
-
-        Map<String,String> user = new HashMap<String,String>();
-        Map<String,String> token = new HashMap<String,String>();
+        Map<String, String> user = new HashMap<String, String>();
 
         user.put("correo",usuarioCorreo);
         user.put("rol",usuarioRol);
@@ -78,16 +84,13 @@ public class AuthController {
 
         if (usuarioLogueado != null){
 
-            String tokenjwt = jwtUtil.create(String.valueOf(id), usuarioCorreo);
-            token.put("jwt", tokenjwt);
+            String token = jwtUtil.create(String.valueOf(id), usuarioCorreo);
+            user.put("token",token);
 
-            map.put("user", user);
-            map.put("token",token);
-
-            System.out.println(map);
-            return map;
+            System.out.println(user);
+            return user;
         }
 
-        return map;
+        return user;
     }
 }
